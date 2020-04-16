7ab3c445f56232fe1638863428827abac8da0386
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2020-04-16 21:41:42 +0200

    add scss plugins to the project

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,19 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
 };
