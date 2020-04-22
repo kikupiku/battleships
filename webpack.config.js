1d81290dd08c8693c7849e52bfeca2913c5add3d
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2020-04-22 13:46:00 +0200

    change space and board dimensions to accommodate firefox

--- webpack.config.js
+++ webpack.config.js
@@ -15,10 +15,13 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
+
           // Creates `style` nodes from JS strings
           'style-loader',
+
           // Translates CSS into CommonJS
           'css-loader',
+
           // Compiles Sass to CSS
           'sass-loader',
         ],
@@ -35,6 +38,16 @@ module.exports = {
           'file-loader',
         ],
       },
+      {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    },
     ],
   },
 };
